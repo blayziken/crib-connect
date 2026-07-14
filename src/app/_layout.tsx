9fc63d39a8f05bbb3bbca7388dd2381796cb8d8a
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2026-07-14 16:37:06 -0400

    feat: integrate Sentry for error tracking and performance monitoring

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,16 @@
 import "../../global.css";
 
 import { Stack } from "expo-router";
+import * as Sentry from "@sentry/react-native";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: "https://0da95e4847783c8dcaf147740f7eb964@o4511735495458816.ingest.us.sentry.io/4511735498014720",
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
+});
+
+function RootLayout() {
   return <Stack />;
 }
+
+export default Sentry.wrap(RootLayout);
